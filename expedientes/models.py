60d4572ff0ff2875abf1bb3ae7c21a2c348c6289
franlopez <franlopez.2001@hotmail.com>
2019-10-15 17:10:16 -0300

    Primeras aproximaciones

--- expedientes/models.py
+++ expedientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Expediente(models.Model):
+    expendiente= models.CharField(max_length=200)
+    fecha_creacion = models.DateTimeField("Fecha de creacion")
+    numero = models.IntegerField()
+    letra = models.CharField(max_length=1)
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    calle = models.CharField(max_length=200)
+	
